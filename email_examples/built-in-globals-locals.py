79cb3801ddc8b08fbbdfc6daedb567d2f2173760
dbrannonhoward <dbrannonhoward@gmail.com>
2020-06-16 12:20:42 -0400

    globals locals builtins

---
+++ email_examples/built-in-globals-locals.py
@@ -0,0 +1,11 @@
+# "globals()" returns a dict
+# with all global variables
+# in the current scope:
+
+print('globals are ' + str(globals()))
+
+# "locals()" does the same
+# but for all local variables
+# in the current scope:
+
+print('locals are ' + str(locals()))
